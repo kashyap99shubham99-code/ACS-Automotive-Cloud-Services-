293cf20a9c18173f5ec0c33e88714e9db530a44c
Shubham Kumar <kashyap99shubham99@gmail.com>
2025-12-21 16:12:16 +0530

    Integrate Eureka server and API Gateway with service discovery and routing

--- eureka-service/src/main/java/com/example/eureka_service/EurekaServerApplication.java
+++ eureka-service/src/main/java/com/example/eureka_service/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.contract_service;
+package com.example.eureka_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ContractServiceApplication {
+public class EurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContractServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
